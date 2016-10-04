943acdd68d044398b4d082d613bc952ac558549d
Arthur Scharf <arthur.scharf@gmail.com>
2016-10-04 08:19:10 +0200

    Initial Commit

---
+++ UE32_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UE32_1
+//
+//  Created by Arthur Scharf on 04/10/2016.
+//  Copyright © 2016 RT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
